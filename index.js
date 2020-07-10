31ade9288e0f643a8fabd7cfac8f6955338beac2
Denis Ershov <denis.ershov.10@gmail.com>
2020-07-10 22:01:26 +0300

    feat: сверстать адаптивную страницу с добавлением js

---
+++ index.js
@@ -0,0 +1,37 @@
+let openPopupButton = document.querySelector('.profile__button-edit');
+let popup = document.querySelector('.popup');
+let closePopupButton = document.querySelector('.popup__close-button');
+let form = popup.querySelector('.form');
+let inputName = document.querySelector('.form__input-name');
+let inputProfession = document.querySelector('.form__input-profession');
+let profileName = document.querySelector('.profile__info-name');
+let profileProfession = document.querySelector('.profile__info-profession');
+
+function togglePopup() {
+  popup.classList.toggle('popup_opened');
+}
+
+
+openPopupButton.addEventListener('click', () => {
+  inputName.value = profileName.textContent;
+  inputProfession.value = profileProfession.textContent;
+
+  togglePopup();
+
+})
+
+closePopupButton.addEventListener('click', togglePopup);
+
+form.addEventListener('submit', (event) => {
+  inputName.value
+  inputProfession.value
+
+  profileName.textContent = inputName.value;
+  profileProfession.textContent = inputProfession.value;
+
+  togglePopup();
+
+  event.preventDefault();
+})
+
+formElement.addEventListener('submit', formSubmitHandler)
