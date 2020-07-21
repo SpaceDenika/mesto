fd0b794e7100a8334f54073d9b5a671c9e739716
Denis Ershov <denis.ershov.10@gmail.com>
2020-07-21 15:15:54 +0300

    feat: добавить второй попап

--- index.js
+++ index.js
@@ -1,15 +1,11 @@
-let openPopupProfileButton = document.querySelector('.profile__button-edit');
+let openPopupButton = document.querySelector('.profile__button-edit');
 let popup = document.querySelector('.popup');
-let closePopupProfileButton = document.querySelector('.popup__close-button');
+let closePopupButton = document.querySelector('.popup__close-button');
 let form = popup.querySelector('.form');
 let inputName = document.querySelector('.form__input_type_name');
 let inputProfession = document.querySelector('.form__input_type_profession');
 let profileName = document.querySelector('.profile__info-name');
 let profileProfession = document.querySelector('.profile__info-profession');
-let inputTitle = document.querySelector('.form__input_type_title');
-let inputLink = document.querySelector('.form__input_type_link');
-let elementText = document.querySelector('.element__text');
-let elementImage = document.querySelector('./element__image');
 
 function togglePopup() {
   popup.classList.toggle('popup_opened');
@@ -19,36 +15,6 @@ function openPopup() {
 
   inputName.value = profileName.textContent;
   inputProfession.value = profileProfession.textContent;
-
-  togglePopup();
-
-}
-
-openPopupProfileButton.addEventListener('click', openPopup);
-
-closePopupProfileButton.addEventListener('click', togglePopup);
-
-function formSubmitHandler (evt) {
-
-  evt.preventDefault();
-
-  profileName.textContent = inputName.value;
-  profileProfession.textContent = inputProfession.value;
-
-  togglePopup();
-
-}
-
-form.addEventListener('submit', formSubmitHandler);
-
-function togglePopup() {
-  popup.classList.toggle('popup_opened');
-}
-
-function openPopup() {
-
-  inputName.value = elementText.textContent;
-  inputProfession.value = elementImage.textContent;
 
   togglePopup();
 
